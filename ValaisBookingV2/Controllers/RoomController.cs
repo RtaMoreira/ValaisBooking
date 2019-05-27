3ec2e170e49e01138ff0e5388186a1bae53ae43d
jcfdsilva <jcfdsilva@gmail.com>
2019-05-27 14:05:02 +0200

    test merge

--- ValaisBookingV2/Controllers/RoomController.cs
+++ ValaisBookingV2/Controllers/RoomController.cs
@@ -9,6 +9,7 @@ namespace ValaisBookingV2.Controllers
 {
     public class RoomController : ApiController
     {
+        string a = "salut";
         private static readonly HttpClient Httpclient;
     }
 }
